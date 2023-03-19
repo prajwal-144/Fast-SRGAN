ab6df52c657d447e9aa1752c353b7d3a473257b6
Anagh Benjwal <anagh.benjwal@learner.manipal.edu>
2023-03-19 17:08:46 +0530

    Update infer.py

--- infer.py
+++ infer.py
@@ -7,6 +7,24 @@ import os
 parser = ArgumentParser()
 parser.add_argument('--image_dir', type=str, help='Directory where images are kept.')
 parser.add_argument('--output_dir', type=str, help='Directory where to output high res images.')
+parser.add_argument('--model_dir', type=str, help='Directory where model is saved.')
+
+def change_model(model, new_input_shape, custom_objects=None):
+    # replace input shape of first layer    
+    model.layers[0]._batch_input_shape = new_input_shape
+
+    # rebuild model architecture by exporting and importing via json
+    new_model = keras.models.model_from_json(model.to_json(),custom_objects=custom_objects)
+
+    # copy weights from old model to new one
+    for layer in new_model.layers:
+        try:
+            layer.set_weights(model.get_layer(name=layer.name).get_weights())
+            print("Loaded layer {}".format(layer.name))
+        except:
+            print("Could not transfer weights for layer {}".format(layer.name))
+
+    return new_model
 
 
 def main():
@@ -16,10 +34,11 @@ def main():
     image_paths = [os.path.join(args.image_dir, x) for x in os.listdir(args.image_dir)]
 
     # Change model input shape to accept all size inputs
-    model = keras.models.load_model('models/generator.h5')
-    inputs = keras.Input((None, None, 3))
-    output = model(inputs)
-    model = keras.models.Model(inputs, output)
+    model = keras.models.load_model(args.model_dir)
+#     inputs = keras.Input((None, None, 3))
+#     output = model(inputs)
+#     model = keras.models.Model(inputs, output)
+    model = change_model(model, new_input_shape=[None, None, None, 3])
 
     # Loop over all images
     for image_path in image_paths:
